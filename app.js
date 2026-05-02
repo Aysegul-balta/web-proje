f09488b995f283a55bf69311bd9a0d22060e148f
Esma Ece Kördeve <kordeveesmaece@gmail.com>
2026-05-02 16:45:13 +0300

    EJS layout yapısı ve public klasör entegrasyonu (#28)

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const path = require('path');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
-// Formdan gelen verileri okuyabilmek için gerekli (Middleware)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
-// Tüm rotalarımızı (URL yollarını) kullanıma açıyoruz
 app.use('/', userRoutes);
 
 const PORT = 3000;
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,21 +1,20 @@
-const path = require('path'); 
-const User = require('../models/userModel'); 
+const User = require('../models/userModel');
 
 // Kayıt sayfasını kullanıcıya gönderir
-exports.getRegisterPage = (req, res) => { 
-    res.sendFile(path.join(__dirname, '../views/register.html')); 
+exports.getRegisterPage = (req, res) => {
+    res.render('register');
 };
 
 // Formdan gelen verileri kaydeder
-exports.registerUser = (req, res) => { 
-    const { username, password } = req.body; 
-    
+exports.registerUser = (req, res) => {
+    const { username, password } = req.body;
+
     // Basit bir kontrol: Alanlar boş mu?
-    if (!username || !password) { 
-        return res.send("Lütfen tüm alanları doldurun."); 
+    if (!username || !password) {
+        return res.send("Lütfen tüm alanları doldurun.");
     }
 
     // Veriyi modele gönderip kaydeder
-    User.save({ username, password }); 
-    res.send(`Kullanıcı ${username} başarıyla kaydedildi!`); 
+    User.save({ username, password });
+    res.send(`Kullanıcı ${username} başarıyla kaydedildi!`);
 };
