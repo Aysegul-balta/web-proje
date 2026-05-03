3a332521d0e4153702fc09ec645aac12d632f6e8
Esma Ece Kördeve <kordeveesmaece@gmail.com>
2026-05-03 18:39:46 +0300

    Ürün detay sayfası geliştirildi (#17)

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,9 +1,25 @@
 const fs = require('fs');
 const path = require('path');
 
-exports.getProducts = (req, res) => {
+const getProductsData = () => {
     const filePath = path.join(__dirname, '../data/products.json');
-    const data = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+    return JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+};
 
-    res.render('index', { products: data });
+exports.getProducts = (req, res) => {
+    const products = getProductsData();
+    res.render('index', { products });
+};
+
+exports.getProductDetail = (req, res) => {
+    const products = getProductsData();
+    const productId = parseInt(req.params.id);
+
+    const product = products.find(item => item.id === productId);
+
+    if (!product) {
+        return res.status(404).send('Ürün bulunamadı');
+    }
+
+    res.render('product-detail', { product });
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 
 router.get('/', productController.getProducts);
+router.get('/products/:id', productController.getProductDetail);
 
 module.exports = router;
