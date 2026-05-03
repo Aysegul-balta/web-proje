3db3e679d7e7071bae3120cedf39b904bf1106ad
Esma Ece Kördeve <kordeveesmaece@gmail.com>
2026-05-03 19:23:45 +0300

    Sepete ekleme fonksiyonu eklendi (#21)

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const session = require('express-session');
 const express = require('express');
 const path = require('path');
 const userRoutes = require('./routes/userRoutes');
@@ -9,6 +10,12 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'bookstore-secret-key',
+    resave: false,
+    saveUninitialized: true
+}));
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/', productRoutes); // ANA SAYFA (ürün listesi)
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,5 +4,6 @@ const productController = require('../controllers/productController');
 
 router.get('/', productController.getProducts);
 router.get('/products/:id', productController.getProductDetail);
+router.post('/cart/add/:id', productController.addToCart);
 
 module.exports = router;
