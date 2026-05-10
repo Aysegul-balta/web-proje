b16ee6eb1bc4a0cb71e09fb83b9d41774aca4bfd
Aysegul <aysegul.balta2002@gmail.com>
2026-05-10 15:40:18 +0300

    main çekiliyor

---
+++ routes/authRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+const authController = require("../controllers/authController");
+
+// register
+router.get("/register", authController.getRegister);
+router.post("/register", authController.postRegister);
+
+// login
+router.get("/login", authController.getLogin);
+router.post("/login", authController.postLogin);
+
+// profile
+router.get("/profile", authController.getProfile);
+
+router.get("/logout", authController.logout);
+
+module.exports = router;
