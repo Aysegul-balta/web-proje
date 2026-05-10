287cc0c4c6cb958c7e2d91babfacedad4fbef5a3
Aysegul <aysegul.balta2002@gmail.com>
2026-05-10 15:41:10 +0300

    main çekiliyor

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,8 +2,19 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
 
+// Ana ürün listesi: /products
 router.get('/', productController.getProducts);
-router.get('/products/:id', productController.getProductDetail);
+
+// Sepet sayfası: /products/cart
+router.get('/cart', productController.getCart);
+
+// Ürün detay sayfası: /products/:id
+
+router.get('/:id', productController.getProductDetail);
+
+// Sepet işlemleri (POST)
 router.post('/cart/add/:id', productController.addToCart);
+router.post('/cart/increase/:id', productController.increaseQuantity);
+router.post('/cart/decrease/:id', productController.decreaseQuantity);
 
 module.exports = router;
