383f6b95330824ef96dbd8e7d4163bf65f2a4ea7
Elaheh Kazemi <kazemii.elaheh@gmail.com>
2026-05-11 22:04:54 +0300

    Show logged in user in header

--- server.js
+++ server.js
@@ -27,9 +27,10 @@ app.use(session({
 app.use(flash());
 
 app.use((req, res, next) => {
-    res.locals.error_msg = req.flash("error_msg");
-    res.locals.success_msg = req.flash("success_msg");
-    next();
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.currentUser = req.session.user;
+  next();
 });
 
 app.use("/", authRoutes);
